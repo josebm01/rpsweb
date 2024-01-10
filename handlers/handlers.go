ea8b5b4b5bc1f03611b9a88c1b52caa4b2f89f47
Jose BM <jjbaxin@sice.com>
2024-01-10 14:51:31 -0600

    Se agrega paquete air para no detener y levantar a cada rato el servidor

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 )
 
+// handler - función encargada de manejar una solicitud HTTP
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Paginal de Inicio")
+	fmt.Fprintln(w, "Pagina de Inicio")
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -8,20 +8,21 @@ import (
 	"rpsweb/handlers"
 )
 
-
-
-
 func main() {
 
 	//? crear enrutador
 	router := http.NewServeMux()
 
-	// handler - función encargada de manejar una solicitud HTTP
+	// rutas y handler - función encargada de manejar una solicitud HTTP
 	router.HandleFunc("/", handlers.Index)
+	router.HandleFunc("/new", handlers.NewGame)
+	router.HandleFunc("/start", handlers.Game)
+	router.HandleFunc("/play", handlers.Play)
+	router.HandleFunc("/about", handlers.About)
 
 	port := ":8080"
 	log.Printf("Servidor escuchando en http://localhost%s\n", port)
-	
+
 	// iniciar el servidor, nil para el manejador de rutas
 	log.Fatal(http.ListenAndServe(port, router))
 }
