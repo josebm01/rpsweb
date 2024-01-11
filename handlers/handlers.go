bae0d19c557b100d5e605995f7c0739114bab112
Jose BM <jjbaxin@sice.com>
2024-01-11 17:27:30 -0600

    Añadiendo vistas html y especificando las rutas en el base.html

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,20 +2,32 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-// handler - función encargada de manejar una solicitud HTTP
+//? Rutas
+const (
+	templateDir = "templates/"
+	templateBase = templateDir + "base.html"
+)
+
+
+
+//? handler - función encargada de manejar una solicitud HTTP
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Pagina de Inicio")
+	// fmt.Fprintln(w, "Inicio")
+	renderTemplate( w, "index.html", nil )
+
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Crear nuevo juego")
+	renderTemplate( w, "new-game.html", nil )
 }
 
 func Game(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Juego")
+	renderTemplate( w, "game.html", nil )
 }
 
 func Play(w http.ResponseWriter, r *http.Request) {
@@ -23,8 +35,29 @@ func Play(w http.ResponseWriter, r *http.Request) {
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Acerca de")
+	renderTemplate( w, "about.html", nil )
 }
 
 
 
+
+//! función genérica para renderizar plantillas
+/**
+	- recibe responseWriter
+	- base de pagina - ruta base desde templates/archivo.html
+	- nombre de la pagina html 
+	- data - información
+**/
+func renderTemplate( w http.ResponseWriter, page string, data any) {
+	//? Cargando plantillas HTML
+	// Must - envuelve función que se encarga de cargar plantillas
+	tpl := template.Must(template.ParseFiles( templateBase, templateDir + page ))
+
+	// Renderizando plantilla y validando error
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error al renderizar plantillas", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+## Para no detener y levantar el servidor por cada cambio que se realice
+> go install github.com/cosmtrek/air@latest
+
+* comprobar versión instalada
+    air -v
+
+* Crear configuración para reiniciar el servidor
+	air init
+
+* Levantar servidor
+	air
